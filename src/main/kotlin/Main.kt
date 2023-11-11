225892cfaa2382f6c5be3b657e3f2823c29f7dfb
Kunle Oyedele <kunle.oyedele@gmail.com>
2023-11-11 16:16:09 -0700

    Fix integer parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,23 @@
 import com.google.gson.Gson;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+import com.dampcake.bencode.Bencode
+import com.dampcake.bencode.Type
 
 val gson = Gson()
 const val IntegerStartTag = 'i'
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println("Logs from your program will appear here!")
     val command = args[0]
     when (command) {
         "decode" -> {
-             // Uncomment this block to pass the first stage
              val bencodedValue = args[1]
-             val decoded = decodeBencode(bencodedValue)
+             val decoded = if (bencodedValue.startsWith("i")) {
+                 Bencode().decode(bencodedValue.toByteArray(), Type.NUMBER)
+             } else {
+                 Bencode().decode(bencodedValue.toByteArray(), Type.STRING)
+             }
              println(gson.toJson(decoded))
              return
         }
         else -> println("Unknown command $command")
-    }
-}
-
-fun decodeBencode(bencodedString: String): String {
-    when {
-        Character.isDigit(bencodedString[0]) -> {
-            val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
-            val length = Integer.parseInt(bencodedString.substring(0, firstColonIndex))
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
-        }
-        bencodedString[0] == IntegerStartTag -> {
-            return bencodedString.substring(1, bencodedString.length - 1)
-        }
-        else -> TODO("Only strings are supported at the moment")
     }
 }
